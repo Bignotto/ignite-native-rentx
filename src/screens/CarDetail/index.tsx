167db3b6d53ef85be9e11d694521ed0821c85099
Thiago Bignotto <bignotto@gmail.com>
2022-03-28 06:55:02 -0300

    implemented car info components and about text styles

--- src/screens/CarDetail/index.tsx
+++ src/screens/CarDetail/index.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
 
-import { CarImages, Container, Header } from "./styles";
+import {
+  About,
+  Brand,
+  CarImages,
+  Container,
+  Content,
+  Description,
+  Details,
+  Header,
+  Name,
+  Period,
+  Price,
+  Rent,
+} from "./styles";
 
 export function CarDetail() {
   return (
@@ -17,6 +30,24 @@ export function CarDetail() {
           ]}
         />
       </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Ferrari</Brand>
+            <Name>F50</Name>
+          </Description>
+          <Rent>
+            <Period>Diária</Period>
+            <Price>R$ 580,00</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   );
 }
