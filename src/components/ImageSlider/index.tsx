aef0a964ef6c59db3ee724c41b5d9fff85648076
Thiago Bignotto <bignotto@gmail.com>
2022-07-04 06:00:19 -0300

    implemented image slider component with index changeing and more than one image

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { FlatList, ViewToken } from "react-native";
 import { CarImage } from "../Car/styles";
 
 import { CarImageWrapper, Container, ImageIndex, ImageIndexes } from "./styles";
@@ -7,20 +8,38 @@ interface Props {
   imagesUrl: string[];
 }
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 export function ImageSlider({ imagesUrl }: Props) {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    const viewableIndex = info.viewableItems[0].index!;
+    setImageIndex(viewableIndex);
+  });
+
   return (
     <Container>
       <ImageIndexes>
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
-        <ImageIndex active={true} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
+        {imagesUrl.map((_, index) => (
+          <ImageIndex active={index === imageIndex} key={index} />
+        ))}
       </ImageIndexes>
-      <CarImageWrapper>
-        <CarImage source={{ uri: imagesUrl[0] }} />
-      </CarImageWrapper>
+      <FlatList
+        data={imagesUrl}
+        keyExtractor={(key) => key}
+        renderItem={({ item }) => (
+          <CarImageWrapper>
+            <CarImage source={{ uri: item }} resizeMode="contain" />
+          </CarImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={indexChanged.current}
+      />
     </Container>
   );
 }
