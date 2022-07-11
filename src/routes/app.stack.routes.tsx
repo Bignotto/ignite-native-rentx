351bec55ff7f0e2d819643bf75d8dc6d28aab279
Thiago Bignotto <bignotto@gmail.com>
2022-07-11 07:20:21 -0300

    implemented input and password input components to use in sign in screen

--- src/routes/app.stack.routes.tsx
+++ src/routes/app.stack.routes.tsx
@@ -7,17 +7,20 @@ import { SchedullingDetails } from "../screens/SchedullingDetails";
 import { SchedullingComplete } from "../screens/SchedullingComplete";
 import MyCars from "../screens/MyCars";
 import Splash from "../screens/Splash";
+import { SignIn } from "../screens/SignIn";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppStackRoutes() {
   return (
     <Navigator
-      initialRouteName="Splash"
+      initialRouteName="SignIn"
       screenOptions={{
         headerShown: false,
       }}
     >
+      <Screen name="SignIn" component={SignIn} />
+
       <Screen name="Splash" component={Splash} />
       <Screen name="Home" component={Home} />
       <Screen name="CarDetail" component={CarDetail} />
